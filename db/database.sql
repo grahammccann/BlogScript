b2e60cb3fec9dcb8f54fec43b2913ad5b565582a
Graham <graham23s@hotmail.com>
2023-07-29 23:41:19 +0100

    Code updates.

--- db/database.sql
+++ db/database.sql
@@ -30,7 +30,7 @@ CREATE TABLE `categories` (
   `category_id` bigint(20) NOT NULL,
   `category_name` varchar(100) NOT NULL,
   `category_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -43,7 +43,7 @@ CREATE TABLE `clicks` (
   `click_page` varchar(250) NOT NULL,
   `click_ip` varchar(39) NOT NULL,
   `click_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `images` (
   `image_alt_text` varchar(100) NOT NULL,
   `image_is_header` enum('no','yes') NOT NULL,
   `image_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -69,7 +69,7 @@ CREATE TABLE `ips` (
   `ip_id` bigint(11) NOT NULL,
   `ip_range` varchar(15) NOT NULL,
   `ip_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -85,7 +85,7 @@ CREATE TABLE `members` (
   `member_email` varchar(50) NOT NULL,
   `member_is_admin` enum('no','yes') NOT NULL,
   `member_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `members`
@@ -104,7 +104,7 @@ CREATE TABLE `newsletters` (
   `newsletter_id` bigint(11) NOT NULL,
   `newsletter_email` varchar(150) NOT NULL,
   `newsletter_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -118,7 +118,7 @@ CREATE TABLE `options` (
   `option_value` longtext NOT NULL,
   `option_description` longtext NOT NULL,
   `option_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `options`
@@ -132,25 +132,25 @@ CREATE TABLE `options` (
 (6, 'homepage_pagination', '5', 'Sets the number of articles displayed on the homepage.', '2021-10-18 21:55:15'),
 (7, 'homepage_show_categories', '0', 'Controls the display of the categories section on the homepage (1=show, 0=hide).', '2021-10-23 20:40:35'),
 (8, 'homepage_hide_login_link', '1', 'Controls the display of the admin login link on the homepage footer (1=hide, 0=show).', '2021-10-29 20:22:40'),
-(9, 'sidebar_cta_1_header', 'CTA1', 'Sets the header for the first sidebar Call To Action (CTA) section.', '2023-01-29 10:59:06'),
-(10, 'sidebar_cta_1_text', 'CTA1 Text', 'Defines the content for the first sidebar CTA section.', '2023-01-29 11:17:34'),
-(11, 'sidebar_cta_2_header', 'CTA2', 'Sets the header for the second sidebar CTA section.', '2023-01-29 11:29:50'),
-(12, 'sidebar_cta_2_text', 'CTA2 Text', 'Defines the content for the second sidebar CTA section.', '2023-01-29 11:31:13'),
-(13, 'hide_all_sidebars', '0', 'Controls the display of sidebars on the site (1=hide, 0=show).', '2023-06-27 11:15:10'),
-(14, 'footer_amazon_disclosure_text', 'Demo Amazon Disclosure', 'Sets the Amazon Associates disclosure text for the site footer.', '2021-10-31 09:00:19'),
-(15, 'footer_twitter', 'https://twitter.com/DemoTwitter', 'Sets the Twitter link for the site footer.', '2021-10-31 09:04:15'),
-(16, 'footer_meta', 'https://www.facebook.com/DemoFacebook/', 'Sets the Facebook link for the site footer.', '2021-10-31 09:06:06'),
-(17, 'footer_instagram', 'https://www.instagram.com/DemoInstagram/', 'Sets the Instagram link for the site footer.', '2021-10-31 09:23:11'),
-(18, 'bottom_link_1', 'https://demo.com/', 'Specifies an additional link to display below the footer.', '2023-02-28 19:09:35'),
-(19, 'google_adsense', 'Demo Adsense Code', 'Sets the Google AdSense code for the <head> tags.', '2023-01-19 10:58:58'),
-(20, 'google_analytics_property_id', 'G-DemoPropertyId', 'Sets the Google Analytics property ID.', '2023-01-21 21:46:26'),
-(21, 'ads_post_top', 'Demo Ad Code', 'Sets the ad code to be displayed at the top of each post page.', '2022-10-09 15:24:42'),
-(22, 'recaptcha2_site_key', '6DemoSiteKey', 'Sets the site key for ReCaptcha2.', '2023-03-12 14:35:56'),
-(23, 'recaptcha2_secret_key', '6DemoSecretKey', 'Sets the secret key for ReCaptcha2.', '2023-03-12 14:36:32'),
-(24, 'about_us_header', 'About Us', 'Sets the header for the "About Us" section.', '2023-02-25 21:02:18'),
-(25, 'about_us_text', 'Demo About Us Text', 'Defines the content for the "About Us" section.', '2023-02-25 21:03:41'),
-(26, 'homepage_introduction_header', 'Demo Header', 'Sets the header for the homepage introduction card.', '2023-01-29 12:05:06'),
-(27, 'homepage_introduction_header_text', 'Demo Intro Text', 'Defines the content for the homepage introduction card.', '2023-01-29 12:20:16'),
+(9, 'homepage_introduction_header', 'Demo Header', 'Sets the header for the homepage introduction card.', '2023-01-29 12:05:06'),
+(10, 'homepage_introduction_header_text', 'Demo Intro Text', 'Defines the content for the homepage introduction card.', '2023-01-29 12:20:16'),
+(11, 'sidebar_cta_1_header', 'CTA1', 'Sets the header for the first sidebar Call To Action (CTA) section.', '2023-01-29 10:59:06'),
+(12, 'sidebar_cta_1_text', 'CTA1 Text', 'Defines the content for the first sidebar CTA section.', '2023-01-29 11:17:34'),
+(13, 'sidebar_cta_2_header', 'CTA2', 'Sets the header for the second sidebar CTA section.', '2023-01-29 11:29:50'),
+(14, 'sidebar_cta_2_text', 'CTA2 Text', 'Defines the content for the second sidebar CTA section.', '2023-01-29 11:31:13'),
+(15, 'hide_all_sidebars', '0', 'Controls the display of sidebars on the site (1=hide, 0=show).', '2023-06-27 11:15:10'),
+(16, 'footer_amazon_disclosure_text', 'Demo Amazon Disclosure', 'Sets the Amazon Associates disclosure text for the site footer.', '2021-10-31 09:00:19'),
+(17, 'footer_twitter', 'https://twitter.com/DemoTwitter', 'Sets the Twitter link for the site footer.', '2021-10-31 09:04:15'),
+(18, 'footer_meta', 'https://www.facebook.com/DemoFacebook/', 'Sets the Facebook link for the site footer.', '2021-10-31 09:06:06'),
+(19, 'footer_instagram', 'https://www.instagram.com/DemoInstagram/', 'Sets the Instagram link for the site footer.', '2021-10-31 09:23:11'),
+(20, 'bottom_link_1', 'https://demo.com/', 'Specifies an additional link to display below the footer.', '2023-02-28 19:09:35'),
+(21, 'google_adsense', 'Demo Adsense Code', 'Sets the Google AdSense code for the <head> tags.', '2023-01-19 10:58:58'),
+(22, 'google_analytics_property_id', 'G-DemoPropertyId', 'Sets the Google Analytics property ID.', '2023-01-21 21:46:26'),
+(23, 'ads_post_top', 'Demo Ad Code', 'Sets the ad code to be displayed at the top of each post page.', '2022-10-09 15:24:42'),
+(24, 'recaptcha2_site_key', '6DemoSiteKey', 'Sets the site key for ReCaptcha2.', '2023-03-12 14:35:56'),
+(25, 'recaptcha2_secret_key', '6DemoSecretKey', 'Sets the secret key for ReCaptcha2.', '2023-03-12 14:36:32'),
+(26, 'about_us_header', 'About Us', 'Sets the header for the "About Us" section.', '2023-02-25 21:02:18'),
+(27, 'about_us_text', 'Demo About Us Text', 'Defines the content for the "About Us" section.', '2023-02-25 21:03:41'),
 (28, 'category_style_icon', 'demo-icon', 'Sets a custom icon for each category name.', '2022-12-18 09:59:54'),
 (29, 'twitter_username', 'DemoTwitterUser', 'Sets the Twitter username for the Twitter cards feature.', '2023-02-09 21:38:34');
 
@@ -167,7 +167,7 @@ CREATE TABLE `pages` (
   `page_name` varchar(100) NOT NULL,
   `page_body` longtext NOT NULL,
   `page_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -193,7 +193,7 @@ CREATE TABLE `posts` (
   `post_affiliate_url` longtext NOT NULL,
   `post_will_show_ads` enum('no','yes') NOT NULL,
   `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -207,7 +207,7 @@ CREATE TABLE `shorteners` (
   `shortener_original_url` longtext NOT NULL,
   `shortener_clicks_count` int(11) NOT NULL,
   `shortener_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
